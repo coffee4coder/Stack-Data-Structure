5500cd4baa33046a7e12b1dc471dbd1a853c1472
Muhammad Talha Wahhab Chy <wchy909@gmail.com>
2024-11-05 12:52:45 +0600

    Added functionality to push user-defined items into Stack

--- Implement-Built-in-Stack.java
+++ Implement-Built-in-Stack.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main{
     public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
         Stack<String> coffeeStack = new Stack<>();
-        
-        coffeeStack.push("GTA V");
-        coffeeStack.push("Cyberpunk 2077");
-        coffeeStack.push("softy alto");
+
+        System.out.print("Number of items to add to the stack: ");
+        int counter = obj.nextInt(); // Number of items user wants to add
+
+        obj.nextLine(); // Consume newline left-over
+
+        for(int i = 0; i < counter; i++){
+            // (i + 1) adjusts i to be based on 1 instead of based on 0.
+            System.out.print("Enter item " + (i + 1) + ": ");
+            String input = obj.nextLine();
+            coffeeStack.push(input);
+        }
         
         System.out.println("Total Size of Stack: " + coffeeStack.size());
         System.out.println("Items in Stack: " + coffeeStack);
