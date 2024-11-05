bb2d910c3a2d0f0153248b654150d51b8107f332
Muhammad Talha Wahhab Chy <wchy909@gmail.com>
2024-11-05 17:32:26 +0600

    Added Necessary Comments

--- Stack-Implementation-Array-Updated.c
+++ Stack-Implementation-Array-Updated.c
@@ -8,37 +8,41 @@
 int stack[Capacity];
 int top = -1;
 
+// Function to add an element to the stack
 void push(int x){
-    if(top < Capacity - 1){
+    if(top < Capacity - 1){ // Check if stack is not full
         top++;
-        stack[top] = x;
+        stack[top] = x; // Add the element to the stack
         printf("Element added:%d\n", x);
     }else{
-        printf("Exception!!, Stack Overflow\n");
+        printf("Exception!!, Stack Overflow\n"); // Handle stack overflow
     }
 }
 
 
+// Function to remove and return the top element from the stack
 int pop(){
     if(top >= 0){
         int holder = stack[top];
         top--;
         return holder;
     }
-    printf("Stack Underflow\n");
+    printf("Stack Underflow\n"); // Handle stack underflow
     return -1;
 }
 
 
+// Function to return the top element without removing it
 int peek(){
     if(top >= 0){
         return stack[top];
     }
-    printf("Stack Underflow\n");
+    printf("Stack Underflow\n"); // Handle stack underflow
     return -1;
 }
 
 
+// Main function to demonstrate stack operations
 int main(){
     int i;
     for(i = 2; i <= 22; i += 2){
